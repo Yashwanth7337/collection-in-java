98fa6e7101887ac1473c4b606eaa8b47b52bf067
yashwanth7337 <yash.yashwanth123@gmail.com>
2023-08-23 10:56:02 +0530

    hi

--- Test.java
+++ Test.java
@@ -22,7 +22,7 @@ public class Test
 {  
     public static void main (String args[])  
     {  
-        Employee e = new Employee(22, "Mukesh", "Delhi", 90000);  
+        Employee e = new Employee(22, "yash", "Bangalore", 90000);  
         System.out.println("Name: "+e.name+" Salary: "+e.salary+" Age: "+e.age+" Address: "+e.address);  
     }  
 }  
