858ef9b51d7f4b59804ac53380e60545d0f2ab2f
yashwanth7337 <yash.yashwanth123@gmail.com>
2023-09-01 12:03:03 +0530

    hi

---
+++ quiz.java
@@ -0,0 +1,10 @@
+public class quiz
+{
+    public static void main(String args[])
+    {
+        String letter="";
+        while(letter.length()!=1)
+        letter+="a";
+        System.out.println(letter);
+    }
+}
