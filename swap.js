6eed1feff92e06826a236485e0dd36bff3b9ae81
yashwanth7337 <yash.yashwanth123@gmail.com>
2023-09-01 11:35:11 +0530

    hello

---
+++ swap.js
@@ -0,0 +1,11 @@
+let a=5;
+let b=2;
+console.log("before swapping a:"+a)
+console.log("before swaping b:"+b)
+let temp;
+temp=a;
+a=b;
+b=temp;
+console.log("after swaping")
+console.log("after swaping a:"+a)
+console.log("after swaping b:"+b)
